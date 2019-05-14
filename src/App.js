fde23b76c7ade5dd15b776225192cbb95182262e
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2019-05-14 16:13:54 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Contact from "./Components/Contact";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Contact />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Contact extends React.Component {
+  render() {
+    return <div>Hey!</div>;
+  }
+}
+
+export default Contact;
